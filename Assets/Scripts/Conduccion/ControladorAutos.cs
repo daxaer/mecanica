4de11f4ef59fa16fa2149e896377607dc2d844ad
Miguelonches <zap407@amerike.edu.mx>
2022-04-26 20:40:33 -0500

    carro

--- Assets/Scripts/Conduccion/ControladorAutos.cs
+++ Assets/Scripts/Conduccion/ControladorAutos.cs
@@ -1,23 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControladorAutos : MonoBehaviour
 {
+
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
 
     private float horizontalInput;
     private float verticalInput;
+    private float steerAngle;
+    private float currentSteerAngle;
+    private float currentBreakForce;
     private bool isBreaking;
 
-    [SerializeField] private float breakForce;
     [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerInAngle;
 
-    [SerializeField] private WheelCollider FrontLeftWheelc;
-    [SerializeField] private WheelCollider FrontRightWheelc;
-    [SerializeField] private WheelCollider RearLeftWheelc;
-    [SerializeField] private WheelCollider ReartRightWheelc;
+    [SerializeField] private WheelCollider frontLeftWheelc;
+    [SerializeField] private WheelCollider frontRightWheelc;
+    [SerializeField] private WheelCollider rearLeftWheelc;
+    [SerializeField] private WheelCollider rearRightWheelc;
+
+    [SerializeField] private Transform frontLeftWheelt;
+    [SerializeField] private Transform frontRightWheelt;
+    [SerializeField] private Transform rearLeftWheelt;
+    [SerializeField] private Transform rearRightWheelt;
 
     private void FixedUpdate()
     {
@@ -36,17 +47,46 @@ public class ControladorAutos : MonoBehaviour
 
     private void HandleMotor()
     {
-        FrontLeftWheelc.motorTorque = verticalInput * motorForce;
-        FrontRightWheelc.motorTorque = verticalInput * motorForce;
+        frontLeftWheelc.motorTorque = verticalInput * motorForce;
+        frontRightWheelc.motorTorque = verticalInput * motorForce;
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        if(isBreaking)
+        {
+            ApplyBreaking();
+        }
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRightWheelc.brakeTorque = currentBreakForce;
+        frontLeftWheelc.brakeTorque = currentBreakForce;
+        rearLeftWheelc.brakeTorque = currentBreakForce;
+        rearRightWheelc.brakeTorque = currentBreakForce;
     }
 
     private void HandleSteering()
     {
-        
+        currentSteerAngle = maxSteerInAngle * horizontalInput;
+        frontLeftWheelc.steerAngle = currentSteerAngle;
+        frontRightWheelc.steerAngle = currentSteerAngle;
+        rearLeftWheelc.steerAngle = steerAngle;
+        rearRightWheelc.steerAngle = steerAngle;
     }
 
     private void UpdateWheels()
     {
-        
+        UpdateSingleWheel(frontLeftWheelc, frontLeftWheelt);
+        UpdateSingleWheel(frontRightWheelc, frontRightWheelt);
+        UpdateSingleWheel(rearLeftWheelc, rearLeftWheelt);
+        UpdateSingleWheel(rearRightWheelc, rearRightWheelt);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
