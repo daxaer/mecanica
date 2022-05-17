3c1a64c4794bad47b5065d750665634dfc858f5b
Leonardo Medellin <Leonardo.Medellin-ext@bigfishgames.com>
2022-05-17 12:24:12 -0500

    Se agrego un dropdown

--- Assets/Scripts/Conduccion/CamaraDeSeguimiento.cs
+++ Assets/Scripts/Conduccion/CamaraDeSeguimiento.cs
@@ -1,31 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CamaraDeSeguimiento : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
-    [SerializeField] private Transform objetivo;
-    [SerializeField] private float velTraslacion;
-    [SerializeField] private float velRotacion;
+    [SerializeField] private Transform[] target;
+    [SerializeField] private float translateSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] TMP_Dropdown dropdown;
+
 
     private void FixedUpdate()
     {
-        Traslacion();
-        Rotacion();
+        ManageVehicle();
     }
 
-    private void Traslacion()
+    public void ManageVehicle()
     {
-        var posObjetivo = objetivo.TransformPoint(offset);
-        transform.position = Vector3.Lerp(transform.position, posObjetivo, velTraslacion * Time.deltaTime);
+        if(dropdown.value == 0)
+        {
+            HandleTranslation(0);
+            HandleRotation(0);
+        }
+        else if (dropdown.value == 1)
+        {
+            HandleTranslation(1);
+            HandleRotation(1);
+        }
+        else if (dropdown.value == 2)
+        {
+            HandleTranslation(2);
+            HandleRotation(2);
+        }
     }
 
-    private void Rotacion()
+    private void HandleTranslation(int _vehicle)
     {
-        var direccion = objetivo.position - transform.position;
-        var rotacion = Quaternion.LookRotation(direccion, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotacion, velRotacion * Time.deltaTime);
+        var targetPosition = target[_vehicle].TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
     }
 
+    private void HandleRotation(int _vehicle)
+    {
+        var direction = target[_vehicle].position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
 }
---
+++ Assets/Scripts/Conduccion/ControladorBus.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorBus : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal2";
+    private const string VERTICAL = "Vertical2";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private float currentSteerAngle;
+    private float currentBreakForce;
+    private bool isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerInAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelc;
+    [SerializeField] private WheelCollider frontRightWheelc;
+    [SerializeField] private WheelCollider rearLeftWheelc;
+    [SerializeField] private WheelCollider rearRightWheelc;
+
+    [SerializeField] private Transform frontLeftWheelt;
+    [SerializeField] private Transform frontRightWheelt;
+    [SerializeField] private Transform rearLeftWheelt;
+    [SerializeField] private Transform rearRightWheelt;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelc.motorTorque = verticalInput * motorForce;
+        frontRightWheelc.motorTorque = verticalInput * motorForce;
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        frontRightWheelc.brakeTorque = currentBreakForce;
+        frontLeftWheelc.brakeTorque = currentBreakForce;
+        rearLeftWheelc.brakeTorque = currentBreakForce;
+        rearRightWheelc.brakeTorque = currentBreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerInAngle * horizontalInput;
+        frontLeftWheelc.steerAngle = currentSteerAngle;
+        frontRightWheelc.steerAngle = currentSteerAngle;
+        rearLeftWheelc.steerAngle = steerAngle;
+        rearRightWheelc.steerAngle = steerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelc, frontLeftWheelt);
+        UpdateSingleWheel(frontRightWheelc, frontRightWheelt);
+        UpdateSingleWheel(rearLeftWheelc, rearLeftWheelt);
+        UpdateSingleWheel(rearRightWheelc, rearRightWheelt);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
